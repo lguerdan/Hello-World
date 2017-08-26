79694e9868313e57981e95e06138d921e4925784
Luke Guerdan <lukeguerdan@Lukes-MacBook-Pro.local>
2017-08-26 12:55:01 -0500

    fix hello world

--- Hello World/HelloWorldViewController.swift
+++ Hello World/HelloWorldViewController.swift
@@ -21,15 +21,15 @@ class HelloWorldViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     @IBAction func helloTapped(_ sender: Any) {
-        messageLabel.text = "Hello World!"
+        messageLabel.text = "Hello World"
     }
     
     @IBAction func clearTapped(_ sender: Any) {
         messageLabel.text = ""
     }
     
-
     /*
     // MARK: - Navigation
 
